a120953468759e5853fe7b7331bc04678964663f
MansillaM <matmansilla1992@gmail.com>
2023-02-17 19:34:09 -0500

    connected to mongoDB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Express = require('express');
 const app = Express();
 const port = process.env.PORT || 3004;
+const MongoManager = require('./src/shared/resources/db/mongodb/mongo-manager')
 
 // Import routes
 const HealthRoutes = require('./src/routes/health.routes');
@@ -11,6 +12,9 @@ const HealthRoutes = require('./src/routes/health.routes');
 app.use(Express.json());
 
 HealthRoutes.registerHealthRoutes(app);
+
+//MongoDB connect
+MongoManager.openMongoConnection();
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
